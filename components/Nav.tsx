fe2af003e699ff99ca6f274bc0b1ea17225dcea3
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-11-21 16:38:44 +0100

    remove nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,9 +1,6 @@
 /* This example requires Tailwind CSS v2.0+ */
-const navigation = [
-  { name: 'Solutions', href: '#' },
-  { name: 'Pricing', href: '#' },
-  { name: 'Docs', href: '#' },
-  { name: 'Company', href: '#' },
+const navigation: any[] = [
+  
 ]
 
 export default function Nav() {
